3da9261f697715595cf20961930143f9ede6bf43
FelipeSDS <felipesilva@fiorde.com.br>
2021-09-24 12:11:46 -0300

    Inserindo annotation post areaController

--- src/main/java/br/com/rconsultancy/controller/area/AreaController.java
+++ src/main/java/br/com/rconsultancy/controller/area/AreaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class AreaController {
 	@Autowired
 	private AreaService areaService;
 	
+	@PostMapping
 	public ResponseEntity<?> create(@RequestBody AreaRequestDTO dto){
 		areaService.save(dto);
 		return new ResponseEntity<>(HttpStatus.CREATED);
